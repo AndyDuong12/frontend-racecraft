288eec76eb4571de87d80793d0ab368d622aa6e2
Anh Duong <168235516+AndyDuong12@users.noreply.github.com>
2025-11-22 19:51:26 -0800

    Removed extra codes in main.jsx
    Removed the first 4 extra lines of codes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,3 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./styles.css";
-
 import React, { StrictMode } from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
 import App from "./App.jsx";
